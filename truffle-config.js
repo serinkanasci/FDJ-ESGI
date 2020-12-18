ff978e0bd7d4540f774433bb0b812948fba4b971
serinkanasci <serinkan.asci7@gmail.com>
2020-12-18 19:12:13 +0100

    Creation Repo

---
+++ truffle-config.js
@@ -0,0 +1,12 @@
+
+const mnemonic = 'dance expire artist core misery illegal reopen lamp soon woman dwarf list';
+
+module.exports = {
+  networks: {
+    development: {
+	    host: "127.0.0.1",     // LOCALHOST (DEFAULT: NONE)
+	    port: 7545,            // STANDARD ETHEREUM PORT (DEFAULT: NONE)
+	    network_id: 5777       // ANY NETWORK (DEFAULT: NONE)
+    }
+  }
+};
